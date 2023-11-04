7a85107d16d31fa97c3a2387696559ff6470e852
Malek-Haba <147167905+Malek-Haba@users.noreply.github.com>
2023-11-04 20:10:05 +0100

    K

---
+++ src/main/java/serveur/ServeurJWS.java
@@ -0,0 +1,10 @@
+package serveur;
+
+import jakarta.xml.ws.Endpoint;
+import service.CalculatriceWS;
+public class ServeurJWS {
+    public static void main(String[] args){
+        Endpoint.publish("http://localhost:8084/",new CalculatriceWS());
+        System.out.println("http://localhost:8084/");
+    }
+}
